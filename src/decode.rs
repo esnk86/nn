9d413a8d7d9e238f15dae9362833598391c015fc
Edward Sankey <esnk86@gmail.com>
2022-05-25 18:22:36 +1200

    add variants for 5XY0 and 9XY0 instructions to Decoded

--- src/decode.rs
+++ src/decode.rs
@@ -11,7 +11,9 @@ pub enum Decoded {
 	MoveIndex(Address),
 	Return,
 	SkipEqual(Register, Byte),
+	SkipEqualXY(Register, Register, Nibble),
 	SkipNotEqual(Register, Byte),
+	SkipNotEqualXY(Register, Register, Nibble),
 
 	Illegal(Instruction),
 }
@@ -30,8 +32,10 @@ pub fn decode(i: Instruction) -> Decoded {
 		0x2 => Decoded::Call(nnn(i)),
 		0x3 => Decoded::SkipEqual(x(i), nn(i)),
 		0x4 => Decoded::SkipNotEqual(x(i), nn(i)),
+		0x5 => Decoded::SkipEqualXY(x(i), y(i), n(i)),
 		0x6 => Decoded::Move(x(i), nn(i)),
 		0x7 => Decoded::Add(x(i), nn(i)),
+		0x9 => Decoded::SkipNotEqualXY(x(i), y(i), n(i)),
 		0xA => Decoded::MoveIndex(nnn(i)),
 		0xD => Decoded::Draw(x(i), y(i), n(i)),
 		_ => Decoded::Illegal(i),
