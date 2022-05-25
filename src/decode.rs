d2a6e845692f6567b57f656ab702fed0696465c9
Edward Sankey <esnk86@gmail.com>
2022-05-26 07:30:38 +1200

    new MoveXY variant of Decoded

--- src/decode.rs
+++ src/decode.rs
@@ -9,6 +9,7 @@ pub enum Decoded {
 	Jump(Address),
 	Move(Register, Byte),
 	MoveIndex(Address),
+	MoveXY(Register, Register),
 	Return,
 	SkipEqual(Register, Byte),
 	SkipEqualXY(Register, Register, Nibble),
@@ -35,6 +36,7 @@ pub fn decode(i: Instruction) -> Decoded {
 		0x5 => Decoded::SkipEqualXY(x(i), y(i), n(i)),
 		0x6 => Decoded::Move(x(i), nn(i)),
 		0x7 => Decoded::Add(x(i), nn(i)),
+		0x8 => a8(i),
 		0x9 => Decoded::SkipNotEqualXY(x(i), y(i), n(i)),
 		0xA => Decoded::MoveIndex(nnn(i)),
 		0xD => Decoded::Draw(x(i), y(i), n(i)),
@@ -64,4 +66,11 @@ fn nn(i: Instruction) -> Byte {
 
 fn n(i: Instruction) -> Nibble {
 	(i & 0xF) as Nibble
+}
+
+fn a8(i: Instruction) -> Decoded {
+	match n(i) {
+		0x0 => Decoded::MoveXY(x(i), y(i)),
+		_ => Decoded::Illegal(i),
+	}
 }
